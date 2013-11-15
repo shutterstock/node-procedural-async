d5303a619e3403aece828b506f3dc418457becc6
David Fenster <dfenster@shutterstock.com>
2013-11-14 19:05:01 -0500

    added array interface

--- bin/test.js
+++ bin/test.js
@@ -11,6 +11,11 @@ Book.findByTitle = function findByTitle(title){
   return b;
 }
 
+Book.findByAuthor = function findByAuthor(author){
+  var b = new Bernhard.generate(Book, true);
+  mysql.query("SELECT MANY THINGS", [author], b.callback);
+}
+
 Book.prototype.author = "unknown";
 
 
@@ -36,8 +41,18 @@ Bernhard.async(function(){
 var mysql = {
   query: function(sql, params, callback){
     console.log("performing query");
-    setTimeout(function(){
-      return callback(null, [{title: params[0], author: "Some Guy"}])
-    }, 3000);
+    if(/SOMETHING/.test(sql)){
+      setTimeout(function(){
+        return callback(null, [{title: params[0], author: "Some Guy"}])
+      }, 1000);
+    } else {
+      setTimeout(function(){
+        return callback(null, [
+          {author: params[0], title: "The Fellowship of the Ring"},
+          {author: params[0], title: "The Two Towers"},
+          {author: params[0], title: "The Return of the King"},
+        ]);
+      }, 3000);
+    }
   }
 }

965dbe8971bbe4d7fa65159a461b4d907c166a35
David Fenster <dfenster@shutterstock.com>
2013-11-14 21:48:06 -0500

    add array tests

--- test/test.js
+++ test/test.js
@@ -1,43 +1,46 @@
 'use strict';
 var Bernhard = require('../lib');
 
-describe("Basic OO", function(){
-  function Book(){
-  }
+function Book(){
+}
 
-  Book.findByTitle = function findByTitle(title){
-    var b = new Bernhard.generate(Book); 
-    mysql.query("SELECT SOMETHING", [title], function(err, results){
-      b.callback(err, results[0]);
-    });
-    return b;
-  }
+Book.findByTitle = function findByTitle(title){
+  var b = new Bernhard.generate(Book); 
+  mysql.query("SELECT SOMETHING", [title], function(err, results){
+    b.callback(err, results[0]);
+  });
+  return b;
+}
 
-  Book.findByAuthor = function findByAuthor(author){
-    var b = new Bernhard.generate(Book, true);
-    mysql.query("SELECT MANY THINGS", [author], b.callback);
-  }
+Book.findByAuthor = function findByAuthor(author){
+  var b = new Bernhard.generate(Book, true);
+  mysql.query("SELECT MANY THINGS", [author], b.callback);
+}
 
-  Book.prototype.author = "unknown";
+Book.prototype.author = "unknown";
 
-  var mysql = {
-    query: function(sql, params, callback){
-      if(/SOMETHING/.test(sql)){
-        setTimeout(function(){
-          return callback(null, [{title: params[0], author: "Some Guy"}])
-        }, 500);
-      } else {
-        setTimeout(function(){
-          return callback(null, [
-            {author: params[0], title: "The Fellowship of the Ring"},
-            {author: params[0], title: "The Two Towers"},
-            {author: params[0], title: "The Return of the King"},
-          ]);
-        }, 1000);
-      }
+var mysql = {
+  query: function(sql, params, callback){
+    if(/SOMETHING/.test(sql)){
+      setTimeout(function(){
+        return callback(null, [{title: params[0], author: "Some Guy"}])
+      }, 500);
+    } else {
+      setTimeout(function(){
+        return callback(null, [
+          {author: params[0], title: "The Fellowship of the Ring"},
+          {author: params[0], title: "The Two Towers"},
+          {author: params[0], title: "The Return of the King"},
+        ]);
+      }, 1000);
     }
   }
+}
 
+
+
+
+describe("Basic OO", function(){
   it('should wait to complete before returning attribute values', function (done){
     Bernhard.async(function(){
       var book = Book.findByTitle('some title');
@@ -59,3 +62,13 @@ describe("Basic OO", function(){
     });    
   })
 });
+
+describe("Arrays", function(){
+  it('should generally work for arrays', function(done){
+    Bernhard.async(function(){
+      var books = Book.findByAuthor('Tolkien');
+      books.length.should.equal(3);
+      done();
+    });
+  })
+})

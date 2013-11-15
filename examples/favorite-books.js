7f18e6728b8ac55ec018ea0516ad8c9962486b4f
Carlos Gomez <cgomez@shutterstock.com>
2013-11-15 12:28:52 -0500

    Changed example code for clarity.

--- examples/favorite-books.js
+++ examples/favorite-books.js
@@ -8,10 +8,12 @@ var res = { json: function(json){ console.log(json); } };
 
 
 Bernhard.async(function(){
+  // Not needed now, don't block.
   var current_user = models.User.retrieveByName(req.session.username);
-  var favorite_book_ids = current_user.retrieveFavoriteBookIds();
   var genre = models.Genre.retrieveByName(req.query.genre);
+  // Will wait on results.
   var book_results = models.Book.search({genre: genre.id});
+  var favorite_book_ids = current_user.retrieveFavoriteBookIds();
   var response_data = book_results.valueOf().map(function(book){
     return {
       id: book.id,

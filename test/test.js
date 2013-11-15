f794192edb19205978b47c9f0e88721febd8845c
David Fenster <dfenster@shutterstock.com>
2013-11-14 19:35:31 -0500

    use real tests

--- test/test.js
+++ test/test.js
@@ -1,12 +1,61 @@
 'use strict';
+var Bernhard = require('../lib');
 
-// TODO: Remove, only here as an example
-describe('addition', function () {
- it('should add 1+1 correctly', function (done) {
-   var onePlusOne = 1 + 1;
-   onePlusOne.should.equal(2);
-   // must call done() so that mocha know that we are... done.
-   // Useful for async tests.
-   done();
- });
+describe("Basic OO", function(){
+  function Book(){
+  }
+
+  Book.findByTitle = function findByTitle(title){
+    var b = new Bernhard.generate(Book); 
+    mysql.query("SELECT SOMETHING", [title], function(err, results){
+      b.callback(err, results[0]);
+    });
+    return b;
+  }
+
+  Book.findByAuthor = function findByAuthor(author){
+    var b = new Bernhard.generate(Book, true);
+    mysql.query("SELECT MANY THINGS", [author], b.callback);
+  }
+
+  Book.prototype.author = "unknown";
+
+  var mysql = {
+    query: function(sql, params, callback){
+      if(/SOMETHING/.test(sql)){
+        setTimeout(function(){
+          return callback(null, [{title: params[0], author: "Some Guy"}])
+        }, 500);
+      } else {
+        setTimeout(function(){
+          return callback(null, [
+            {author: params[0], title: "The Fellowship of the Ring"},
+            {author: params[0], title: "The Two Towers"},
+            {author: params[0], title: "The Return of the King"},
+          ]);
+        }, 1000);
+      }
+    }
+  }
+
+  it('should wait to complete before returning attribute values', function (done){
+    Bernhard.async(function(){
+      var book = Book.findByTitle('some title');
+      book.author.should.equal("Some Guy");
+      done();
+    });
+  });
+  it('should allow for default values without waiting', function (done){
+    Bernhard.async(function(){
+      new Book().author.should.equal("unknown");
+      done();
+    });
+  });
+  it('should be an instance of the original class', function(done){
+    Bernhard.async(function(){
+      var book = Book.findByTitle('some title');
+      book.should.be.an.instanceof(Book);
+      done()
+    });    
+  })
 });
